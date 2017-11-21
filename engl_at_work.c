04f4359cadab6f3dfdcb3606491fa0c3d19e5f43
JohnCodo <ioan.codospan@gmail.com>
2017-11-21 21:50:24 +0200

    Added first version

---
+++ engl_at_work.c
@@ -0,0 +1,10 @@
+ENGLISH PHRASES
+
+Thank you for your kind offer but I will have to decline
+ Where things have got rather dramatic
+Excuse me , do you know where the check in desks for flights to Sydney are ?
+Oh yeah, over there luv- desk number 47
+I’ve been doing a lot of thinking
+I didn’t realize how much I missed you until you had gone
+Why should I after all you ‘ve put me through?
+
