994cda61344b415d070480331917be13751df865
JohnCodo <ioan.codospan@gmail.com>
2018-01-24 16:40:55 +0200

    Adding the first version of comp

---
+++ comp.c
@@ -0,0 +1,16 @@
+//  something about Romania
+printf("1....the first");
+
+/*Romania is located in Southeastern Europe and has its Capital in the city of Bucharest. It borders the Black Sea, 
+ Bulgaria, Ukraine, Hungary, Serbia and Moldova.
+ It has an area of 238397(two hundred thirty eight thousand three hundred ninety seven) square kilometers and a 
+ temperate continental climate. So, Romania has almost 20 million inhabitants and from this point
+ of view the country is the seventh most populous member state of the European Union. I live in Timisoara which is 
+ situated in east side of the country on the Bega river.
+ Timisoara is third most populous city in the country with approximately 320.000 inhabitants. 
+ In 2021 this beautiful city was selected as the European Capital of Culture.
+ So, in Timisoara, there are a lot of multinational IT companies like Intel, Continental, Nokia and Athos which
+ developer a lot of IT products.
+ A year ago I got hired at the Continental company , but in the past I worked for two other small companies.
+ I have a lot of problems with my English, but I hope to overcome this and to be better in what I do.
+*/
