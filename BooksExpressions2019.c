854945a7ce86118b0f4c947602ebf5ccd668c36f
JohnCodo <ioan.codospan@gmail.com>
2020-05-10 19:27:06 +0300

    added firs version of german

---
+++ BooksExpressions2019.c
@@ -0,0 +1,68 @@
+/*##############################################*/ 1.  FIFTY SHADES OF GREY - EL JAMES /*##############################################*/
+/** 
+1. I shake my head, disturbed at the direction of my thoughts and retrieve her question from my backpack=
+2. When I pluck up the courage to look at him=
+3. One hand relaxed in his lap and the other cupping his chin and trailing his long index finger across his lips=
+4. To suppress a smile=
+5. I shutter=
+6. I flash=
+7. He relents = 
+8. I frown, dragging my wayward attention back to the task at hand=
+9. I smooth a stray lock of hair behind my ear=
+10. I glance up at him=
+11. His smile is rueful=
+12. My belief is to achieve success in any scheme one has to make oneself master of that scheme, know inside and out, know every detail.
+I work hard , very hard to do that. I make decisions based and logic and facts. I have a natural gut instinct that can spot and nurture a good solid 
+idea and good people. The bottom line is it's always down to good people.
+13. It really is all about having the right people on your team and directing their energies accordingly=
+14. The growth and development of people is the highest calling of leadership=
+15. His overwhelming good looks maybe=
+16. My gaze steadily=
+17. The way his eyes blaze at me =
+18. Control freak=
+19. Beside, immense power is acquired by assuring yourself in your secret reveries that you were born to control things=
+20. My mouth drops open. I am staggered by his lack of humility
+21. But holy crap, he's arrogant. I change tack=
+22. His eyes are alight with some wicked thought=
+23. What do you do to chill out?= 
+24. I sail, I fly, I indulge in various physical pursuits=
+25. His lip curls in a wry smile=
+26. It's shrewd business=
+27. If so, what is it?= 
+28. A man who acquires the ability to take full possession of his own mind may take possession of anything else to which he is justly entitled=
+29. You were adopted. How much do you think that's shaped the way you are? Oh , this is personal. I stare at him, hoping he's not offended.
+His brown furrows=
+30. I have no way of knowing. My interest is piqued=
+31. He's terse . I squirm=
+32. He's made me feel like an errant child=
+33. Have you had to sacrifice family life for your work ?
+34. I'm not interested in extending my family beyond that=
+35. Nervously, I tuck my loosened hair behind my ear=
+36. He coocks his head to one side=
+37. The blood drains from my head=
+38. She compiled the questions=
+39. My face is aflame=
+40. He rubs his chin in quiet deliberation, his gray eyes appraising me. Gapping at him=
+41. Hang on, who's supposed to be interviewing whom? His eyes burn into me, and I'm compelled to answer with the truth.
+42. I was drafted=
+43. She mutters. I shrug , thrown by his interest=
+44. I'll that in mind=
+45. I'm musing out loud again=
+46. He tilts his head to one side,intrigued, a hint of a smile playing on his lips=
+47. I'm uncoordinated, scruffy, and I'm not blonde =
+48. I lean forward to retrieve the recorder=
+49. His eyes narrow=
+50. The pleasure's been all mine, he says polite as ever. I nod at him=
+51.
+52.
+53.
+54.
+55.
+56.
+57.
+58.
+59.
+60.
+
+
+/
---
+++ german/duo/german_duo.c
@@ -0,0 +1,111 @@
+
+day1 
+Junge = boy
+Eine Frau= A woman
+"Eine" means "one"/"a"/"an", and it is used for feminine nouns (the -e ending is also for plural, but there's no "eine" for plural of course). 
+Masculine and neuter nouns rather take "ein". These endings apply to the nominative case (subject of clause).
+If you want the number one, it is "eins".
+Mann= man
+Ich bin John=I am John
+Eine Frau/Ein Mann
+Mädchen= girl
+I bin ein Kind und du bist ein Mann.
+Sie sind eine Frau und ich bin ein Mann.=You are a woman and I am a man.
+Ich bin ein Mädchen
+Brot= bread
+Ein Mann trinkt.= A man drinks
+Es ist Wasser= It is water
+Sie trinkt = She drinks
+Die Frau= the woman 
+Das wasser= the water
+Der Mann trinkt
+
+die Stadt= the town
+die Straße= the street
+die Adresse= the address
+die Nationalität=the nationality
+das Land=the country
+die Telefonnummer=the phone number
+Attention! Nouns in German always begin with a capital letter.
+
+Guten Tag, wie heißen Sie?
+Hello, what's your name?
+Ich heiße Phillip Stark.
+My name is Phillip Stark.
+Wie ist Ihre Adresse? Bitte tragen Sie hier die Straße und die Stadt ein, wo Sie wohnen.
+What's your address? Please fill in here the street and city where you live.
+Gut, dann trage ich bei Land Deutschland ein. Sind Sie Deutscher? Oder haben Sie eine andere Nationalität?
+Good, then I'll fill in Germany under country. Are you German? Or do you have a different nationality?
+Gut, dann sind wir fast fertig. Wie ist Ihre Telefonnummer?
+Good, then we're nearly done. What is your telephone number?
+069 92887880.
+Wohnen means to live in the sense of to reside. You can say wohnen instead of leben, when talking about where you live.
+ich wohne
+du wohnst
+er/sie/es wohnt
+wir wohnen
+ihr wohnt
+sie/Sie wohnen
+heißen
+Instead of mein Name ist, you'll often hear ich heiße in German, which literally means I am called.
+ich heiße=I am called
+du heißt
+er/sie/es heißt
+wir heißen
+ihr heißt
+sie/Sie heißen
+Ich wohne in dieser Stadt.
+I reside in this town.
+Meine Adresse ist Hamburger Straße 3.
+My address is Hamburger Straße 3.
+Wie ist deine Telefonnummer?
+What's your telephone number?
+Ich heiße Sabrina, wie heißt du?
+I'm called Sabrina, what are you called?
+Tragen Sie das Land, wo Sie wohnen, und Ihre Nationalität ein.
+Fill in the country where you reside, and your nationality.
+/** other else*/
+der Apfel=the apple
+Wir essen Brot= we are eating bread
+Die Frauen essen = the women eat
+Ihr esst Brot= You are eating bread
+Das Mädchen isst einen Apfel=The girl is eating an apple.
+die Zeitung=the newspaper
+du liest= you read
+Das Mädchen liest eine Zeitung= the girl is reading a newspaper
+das Buch=the book
+die Bücher=the books
+Die Kinder lesen Bücher=the children read books
+Die Männer und die Frauen lesen=the men and the women read
+Sie hat einen Apfel= she has an apple
+Wir essen den Apfel=we are eating the apple
+Der Mann isst den Apfel= the man eats the apple
+Er hat die Bücher= he has the books
+Das Kind trinkt die Milch= the child is drinking the milk
+Has du ein Buch= Do you have a book ?
+Wir haben Wasser= we have water
+Ihr habt einen Apfel= you have an apple
+/** INTRO */
+Heißt du Anna?= are you called Anna?
+Wir	heißen Anna und Lukas= our names are called Anna and Lukas
+kommen=come
+Sie kommt aus Deutschland= she comes from Germany.
+Kommst du aus Deutschland?= are you from Germany?
+Kommst du aus Frankreicht?= are you from France?
+Sie kommt aus England= sie is from England
+Sie verstehen= they understand
+Sie versteht=she understantds
+Sprichst du Deutsch? = Do you speak German?
+/** FOOD */
+der Fisch= the fish
+die Suppe= the soup
+das Essen= the food
+Das Mädchen isst Suppe[zupe]= the girl is eating soupp
+Du isst Eis= you are eating ice cream
+der Kaffee=the coffee
+das Bier= the beer
+der Wein(Vain)= the wine
+Die Pizza schmeckt gut = the pizza tastes good 
+Hast du Durst= are you thirsty?
+Sie hat Durst= she is thirsty
+Sie trinken Tee und sie trinkt Kaffee= they are drinking tea and she is drinking coffee
