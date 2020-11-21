b00bc24cb6e4e5817563d9bebc02eb4f8ea8e75c
JohnCodo <ioan.codospan@gmail.com>
2020-11-21 17:05:48 +0200

    commit

---
+++ list_of_Engl_verbs2020.c
@@ -0,0 +1,325 @@
+
+
+
+//Verbs and phrases
+
+//A
+1.accuse 	    : Tom accused me of lying
+2.achieve	    : She achieved remarkable results
+3.aknnowledge	: He acknowledged receiving assistance
+4.acquire		: Meg acquired many new friends
+5.adjust		: You will soon adjust to living in a dormitory
+6.admire		: I admire your confidence
+7.admit			: He was embarrassed to admit making a mistake 
+8.adopt			: I liked your idea and adopted it.
+9.advise		: He advised applying at once.
+10.afford 		: I can't afford to spend any more money this week
+11.agree		: Why did you agree to meet her in the first place
+12.aim			: We aim to increase the speed of delivery.
+13.allow		: Swimming isn’t allowed here.
+14.announce		: She announced her intention to retire.
+15.appear		: Jack appears to be tired today.
+16.apply		: Tom applied for a leave of absence.
+17.appreciate	: I appreciate having a trouble with his supervisor.
+18.approach		: She approached him with a smile on her face.
+19.approve		: I don’t think Tom would approve.
+20.argue		: I don’t want to argue with you.
+21.arise		: The problem has arisen simply because you didn’t follow my instructions.
+22.arrange		: Have you arranged to meet Mark this weekend?
+23.arrive		: We arrived home late.
+24.ask			: Historians frequently ask to consult the collection.
+25.assume		: I assume Tom didn’t show up.
+26.assure		: I assure you Tom will be perfectly safe.
+27.astonish		: I was astonished by his ignorance..
+28.attach		: You need to attach your photo to the application form.
+29.attempt		: Are you going to attempt to pass the exam?
+30.attend		: She attends school at night.
+31.attract		: Tom certainly attracted a lot of attention.
+32.avoid		: She decided to be a nun in order to avoid meeting him.
+33.awake		: Tom awoke at daybreak.
+
+//B
+1.bake			: Tom baked some muffins.
+2.bathe			: I bathe every day.
+3.be			: He is immature.
+4.bear			: I wish she wouldn’t eat so fast. I can’t bear watching her.
+5.beat			: You can’t beat me.
+6.become		: John became very sick.
+7.beg			: I beg to differ with you.
+8.begin			: The leaves begin to fall when autumn comes.
+9.behave		: Tom always behaves himself well. However, Mary does not.
+10.believe		: I believe you’re right.
+11.belong		: This bicycle belongs to me.
+12.bend			: Lie flat and let your knees bend.
+13.bet			: I bet you know French.
+14.bind			: Do you bind books?
+15.bite			: I got bitten by mosquitoes.
+16.blow			: Tom blew himself up accidentally.
+17.boil			: Please boil an egg for me.
+18.borrow		: I need to borrow your car.
+19.bounce		: Bounce the ball and try and hit it over the net.
+20.bow			: Every child bowed to the teacher.
+21.break		: We broke up.
+22.breed		: Rabbits breed quickly.
+23.bring		: I brought some dessert.
+24.broadcast	: We broadcast news on the hour.
+25.build		: We need to build a fire.
+26.burn			: The spy burned the papers.
+27.burst		: John burst into the room.
+28.buy			: I’ll buy a lot of candies for you.
+
+//C
+1.calculate		: A computer can calculate very rapidly.
+2.can/Could		: Can you give me a ring at about 10?
+3.care			: Would you care to join us for dinner?
+4.carry			: I don’t carry cash anymore.
+5.catch			: Let’s catch a bite.
+6.celebrate		: We’re celebrating Tom’s birthday.
+7.change		: I changed my mind.
+8.choose		: Every day is beautiful if you choose to see it.
+9.chop			: Tom chopped down the tree that was in our front yard.
+10.claim		: This diet claims to eliminate toxins from the body.
+11.climb		: Carlos climbed the mountain.
+12.cling		: The mud clung to his shoes.
+13.come			: I’m coming today.
+14.commit		: David didn’t commit those crimes.
+15.communicate	: I can’t communicate with Anna like I used to.
+16.compare		: They compared the new car with the old one.
+17.compete		: I competed with him for the first prize.
+18.complain		: John complained about the weather.
+19.complete		: He completed drawing his pictures.
+20.concern		: I’m concerned for Anna’s safety.
+21.confirm		: The report has yet to be confirmed.
+22.consent		: We hope you will consent to act in his stead.
+23.consider		: Investors should consider putting some money into an annuity.
+24.consist		: A soccer team consists of eleven players.
+25.consult		: You’d better consult your doctor.
+26.contain		: This box contains five apples.
+27.continue		: The finance minister will continue to mastermind Poland’s economic reform.
+28.convince		: I’m not totally convinced of that.
+29.cook			: The pizza will then take about twenty minutes to cook.
+30.cost			: It’ll cost about 10,000 yen.
+31.count		: We’re counting on you.
+32.crawl		: Tom crawled into bed just before midnight.
+33.create		: I have to create a new website.
+34.creep		: We crept toward the enemy.
+35.criticize	: Tom criticized Mary for not doing the job correctly.
+36.cry			: The baby is crying.
+37.cut			: John cut his finger.
+
+//D
+1.dance: I want to dance.
+2.dare: He didn’t dare to speak to her.
+3.deal: I have to dealt with it.
+4.decide: He has decided to live in France.
+5.defer: She deferred writing my thesis.
+6.delay: Big companies often delay paying their bills.
+7.deliver: Letters are delivered every day.
+8.demand: I demand to know what’s going on.
+9.deny: She denied taking the money.
+10.depend: I can’t depend on you anymore.
+11.describe: John can’t describe how painful it was.
+12.deserve: They didn’t deserve to win.
+13.desire: We all desire success.
+14.destroy: John’s house was destroyed by a hurricane.
+15.determine: I am determined to carry out this plan.
+16.develop: Swimming develops our muscles.
+17.differ: My opinion differs from yours.
+18.disagree: It pains me to disagree with your opinion.
+19.discover: The miner discovered a valuable pocket of gold.
+20.discuss: We briefly discussed buying a second car.
+21.dislike: I dislike being the centre of attention.
+22.distribute: The teacher distributed the leaflets.
+23.dive: John learned to dive when he was five.
+24.do: I don’t know.
+25.doubt: I doubt if it’ll snow.
+26.drag: I had to drag him out of bed.
+27.dream: I dreamt about you.
+28.drill: They intended to drill for oil.
+29.drink:  Can I have something to drink?
+30.drive: He drives a truck.
+31.drop: I dropped my sandwich.
+32.dry: Raisins are dried grapes.
+
+//E
+Earn: He earns three times more than me.
+Eat:  You can’t eat your cake and have it.
+Emphasize: I want to emphasize this point in particular.
+Enable: His wealth enables him to do anything.
+Encourage: John encouraged Mary to learn how to speak French.
+Engage: We used to be engaged.
+Enhance: Can we enhance the image?
+Enjoy: I really enjoy talking to you.
+Ensure: This medicine will ensure you a good night’s sleep.
+Entail: This review procedure entails repeating the test.
+Enter: He entered the room.
+Establish: The school was established in 1650.
+Examine: The doctor examined the patients.
+Exist: I don’t believe such things to exist.
+Expand: The workers are expanding the road.
+Expect: What time do you expect to arrive home?
+Experiment: They’re experimenting with a new car.
+Explain: I can e
+xplain everything.
+Explore: He explored the Amazon jungle.
+Extend: We extended a hearty welcome to them.
+
+//F
+Fail: I fail to comprehend their attitude.
+Fall: I fell in the pool.
+Feed: We just fed the baby.
+Feel: I feel that Mr. Peter is a good teacher.
+Fight: Don’t fight with me.
+Find: I can find them.
+Finish: He finished cleaning the kitchen.
+Fit: This coat doesn’t fit me.
+Fly: Tom wishes he could fly.
+Fold: Tom and Mary folded up the flag.
+Follow: We must follow the rules of the game.
+Forbid: I forbid you to smoke.
+Forget: I’ll never forget visiting them.
+Forgive: We have already forgiven you.
+Freeze: It’s freezing cold in this country.
+Fry: She fried fish in salad oil.
+
+//G
+Generate: This machine generates electricity.
+Get: We’ve got to get the economy under control or it will literally eat us up.
+Give:  The waiter gives me the menu.
+Go: Let’s go eat.
+Grind: We grind our coffee by hand.
+Grow: Apples grow on trees
+
+//H
+Hang: Don’t you hang up on me.
+Happen: You made it happen.
+Hate: I hate getting to the theatre late.
+Have: I have a car.
+Hear: I will hear me.
+Hesitate: I hesitate to spend so much money on clothes.
+Hide: I’m hiding from Tim.
+Hit: I hit the jackpot.
+Hold:  Hold the knife at an angle.
+Hop:  I tried to hop on my good foot while holding onto Jim…
+Hope: I hope to see you again soon.
+Hug: I really need a hug.
+Hurry: It had to hurry to find a home because I was already on to something else.
+Hurt: I hurt my elbow.
+
+//I-J
+Identify: She identified him as the murderer.
+Ignore: He ignored her advice.
+Illustrate: The teacher will illustrate how to do it.
+Imagine: I can imagine how you felt.
+Imply: Silence implies consent.
+Impress: We’re not impressed.
+Improve: I need to improve my French.
+Include: Tom’s lunch includes a sandwich and an apple.
+Incorporate: Her business was incorporated.
+Indicate: The arrow indicates the way to go.
+Inform: I’ll inform John about our decision.
+Insist: She insisted on going there.
+Install: The man tried to install his own antenna.
+Intend: I heard they intend to marry.
+Introduce: I’ll introduce you to Tom.
+Invest: He invested his money in stocks.
+Investigate: I came here to investigate Tom’s death.
+Involve: This procedure involves testing each sample twice.
+Iron: I iron my clothes almost every day.
+Jog: I make it a rule to jog every morning.
+Jump:  Can you jump over the river?
+Justify: My results justify taking drastic action.
+
+//K
+Keep: I keep thinking about Joe, all alone in that place.
+Kick:  The kids love to kick a ball against my wall.
+Kiss: Did you kiss anybody?
+Kneel:  Do not run, stand, kneel or spin in the slide.
+Knit: She knit him a sweater for his birthday.
+Know: We know him.
+
+//L
+Lack: Tom seems to lack energy.
+Laugh: Tom is laughing.
+Lay: He laid on his back.
+Lead: Tom leads a quiet life.
+Lean: He leaned on his elbows.
+Leap: Ken leapt over the wall.
+Learn: Children learn to creep ere they can go.
+Leave: Leave me alone!
+Lend: Tom lent Mary his camera.
+Lie (in bed): Lie back down.
+Lift:  He couldn’t lift the table and no more could I.
+Light:  Better to light one candle than to curse the darkness.
+Lie (not to tell the truth): He hated lying.
+Like: She likes playing tennis.
+Listen: Why won’t you listen?
+Look: It looks cold outside.
+Lose: She lost a book.
+Love: I love going out to restaurants.
+
+//M-N
+Maintain: Tom maintained eye contact with Mary.
+Make: I’m making tea.
+Manage: Did you manage to catch the post?
+Matter: It doesn’t matter, Tom.
+May: Each nurse may be responsible for up to twenty patients.
+Mean: I didn’t mean to hurt your feelings.
+Measure: The surfboard measures 2 meters by 55 centimeters.
+Meet: We’ve never met.
+Melt: The snow is melted.
+Mention: He mentioned going to that college.
+Might: Donna might be able to come tomorrow, but it’s very unlikely.
+Mind: Would you mind repeating what you just said?
+Miss: He had missed being elected by a single vote.
+Mix: If you mix blue and red, you get violet.
+Mow: I mowed Tom’s lawn.
+Must: I really must get some exercise.
+Need: You need to change your eating habits.
+Neglect: Don’t neglect to lock the door when you leave.
+Negotiate: The two countries negotiated a treaty.
+
+//O
+Observe: You must observe those rules.
+Obtain: I obtained the painting at an auction.
+Occur: The accident occurred yesterday morning
+Offer: She offered to help me move my things to my new house.
+Open: Open the windows.
+Operate: I can’t figure out how to operate this machine.
+Order:  What do you suggest I order?
+Organize: They want me to organize the party.
+Ought to: You ought to get your watch repaired.
+Overcome: We have to overcome many difficulties.
+Overtake: Their car overtook ours.
+Owe: Tom owes me money.
+Own: I own a German car.
+
+//P
+Paint: She painted the wall pink.
+Participate: He participated in the debate.
+Pay: Can I pay by installment payment?
+Peel: Anna peeled the apple.
+Perform: Tom performs in a jazz club three nights a week.
+Persuade: I persuaded Tom to help me.
+Pinch: He pinched and scraped for many years to save money.
+Plan: Next year I plan to travel around the world.
+Play: I can play tennis.
+Point: Tom pointed to the sky.
+Possess: The old man possesses great wealth.
+Postpone: He postponed returning to Paris.
+Pour: She poured tea for me.
+Practice: Today we’re going to practice parking.
+Prefer: Chantal prefers travelling by train.
+Prepare: The doctor prepared to prescribe a receipt.
+Pretend: She was pretending to cry. I knew she was lying.
+Prevent: The rain prevented me from coming.
+Proceed: They will proceed to build another laboratory building.
+Promise: He promised to collect her from the airport.
+Propose: We propose to deal with this subject in the following chapter.
+Protect: We’re supposed to be protecting John.
+Prove: I’ll prove it to you.
+Pull: John pulled out a pen.
+Punch: You punch like a girl.
+Pursue: The police pursued the murderer.
+Push: We had to push our way through the crowd.
+Put: I put on my shoes.
